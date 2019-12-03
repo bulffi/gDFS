e5b67a00346fbcdd5752a3863a2afeae61f10415
Zijian Zhang <39160610+bulffi@users.noreply.github.com>
2019-12-03 10:03:44 +0800

    将配置文件放到 jar 包外侧 /conf 文件夹内

--- dataNode/src/main/java/server/FileServer.java
+++ dataNode/src/main/java/server/FileServer.java
@@ -27,13 +27,30 @@ public class FileServer {
     private final CountDownLatch mainThreadLatch;
     private final AtomicLong availableBlockID;
 
+    private String getPath()
+    {
+        String path = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+        if(System.getProperty("os.name").contains("dows"))
+        {
+            path = path.substring(1,path.length());
+        }
+        if(path.contains("jar"))
+        {
+            path = path.substring(0,path.lastIndexOf("."));
+            return path.substring(0,path.lastIndexOf("/"));
+        }
+        return path.replace("target/classes/", "");
+    }
+
+
     // 初始化 查找现在能写的最大的 ID 是多少
     public FileServer(CountDownLatch latch) throws IOException {
         Properties properties = new Properties();
+        String pathForConf = getPath() + "/conf/dataNodeConfig.properties";
         try {
-            properties.load(FileServer.class.getResourceAsStream("/dataNodeConfig.properties"));
+            properties.load(new FileReader(new File(pathForConf)));
         }catch (IOException e){
-            logger.error("Fail to read dataNodeConfig.properties. Data node won't start.");
+            logger.error("Fail to read dataNodeConfig.properties in "+ pathForConf +" . Data node won't start.");
             throw e;
         }
         try {
